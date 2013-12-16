ce0c0d81998e51787168f3681f720cf0e0672498
Javier Belmonte <javier.belmonte@hesge.ch>
2013-12-16 15:46:36 +0100

    Uses the new Proxy capabilities of the PHP Broker to implement an autocomplete that would work on new server

--- autocomplete.php
+++ autocomplete.php
@@ -1,7 +1,14 @@
 <?php
 
+define('ROOT', getcwd() . DIRECTORY_SEPARATOR);
+
+define('CONFIG', ROOT . 'config' . DIRECTORY_SEPARATOR);
+
 define('TOOLS', 'tools' . DIRECTORY_SEPARATOR);
 define('RODINCLIENT', TOOLS . 'rodin-client' . DIRECTORY_SEPARATOR);
+
+// Load basic configuration
+include_once(CONFIG . 'base.inc.php');
 
 include_once(RODINCLIENT . 'RodinBroker.php');
 
@@ -13,7 +20,9 @@ if ($query != '') {
 	// $url = "http://suggestqueries.google.com/complete/search?client=firefox&q=" . $query;
 	$url = "http://82.192.234.100:25834/-/rodin/xxl/app/u/AutoCompleteResponder.php?setversion=2012&user_id=11&query=" . $query;
 
-	$response = RodinBroker::makeCall(RodinBroker::METHOD_GET, $url);
+	$proxy = $useProxy ? $proxyConfig : null;
+	
+	$response = RodinBroker::makeCall(RodinBroker::METHOD_GET, $url, null, $proxy);
 
 	if ($response->code == 200) {
 		$suggestions = $response->body->data;
